dbd2197b4f465538154fb5b448bad077a5ae4268
Exercícios.py2 <joaorafaelsantos17@gmail.com>
2025-03-04 14:55:14 -0300

    continuação dos exercícios

---
+++ exercícios.py
@@ -0,0 +1,26 @@
+#print('hello world')
+
+#num= (int(input('Digite um numero: ')),
+   #   int(input('Digite um numero: ')),
+    #  int(input('Digite um numero: ')),
+     # int(input('Digite um numero: ')))
+#print(f'Voçe digitou os valores {num}')
+#if 9 in num:
+ #   print(f'O valor 9 apareceu {num.count(9)} vezes')
+#else:
+ #   print('O numero 9 nao foi encontrado')
+#if 3 in num:
+ #   print(f'O numero 3 apareceu na posição {num.index(3)+1}')
+#else:
+ #   print('O valor 3 nao foi encontrado')
+#for n in num:
+ #   if n%2==0:
+  #      print(f'O numero {n} é par')
+
+#lista
+valores=[]
+valores.append(5)
+valores.append(10)
+valores.append(15)
+for c,v in enumerate(valores):
+    print(f'Na posição {c} econtrei o valor {v}!')
