2d3c7a2adce05a3678193244ddaa1a9d8225f2c7
Exercícios.py2 <joaorafaelsantos17@gmail.com>
2025-03-11 19:36:59 -0300

    atualizações

---
+++ ex004.py
@@ -0,0 +1,37 @@
+#dicionary = a collection of {key:value} pairs
+#            ordered and changeable.No duplicates
+
+capitals = {"USA":"Washington",
+            "Brasil":"Brasilia",
+            "Russia":"Moscow",
+            "China":"Beijing"}
+
+#print(capitals.get("Brasil"))
+
+#if capitals.get("Brasil"):
+#    print("That capital exists")
+#else:
+#    print("That capital doesn't exist")
+
+#for state, capital in capitals.items():
+# print(f"{state} has the capital {capital}")
+
+#capitals.update({"Argentina":"Buenos Aires"})
+#.update tb serve para "atualizar"/ capitals.update({"USA":"Detroit"})
+#capitals.pop("China") remove
+#capitals.clear()
+#print(capitals)
+
+#keys = capitals.keys()
+#for key in capitals.keys():
+#print(keys)
+
+#values = capitals.values()
+#for value in capitals.values():
+#    print(value)
+
+#items = capitals.items()
+#for key, value in capitals.items():
+#    print(f"{key} has the capital {value}")
+
+
